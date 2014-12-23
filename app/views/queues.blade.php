3433cddf6c0754fa4ef2ea3b36979141556f5782
vince <vince@aspendigital.com>
2014-12-22 21:41:18 -0700

    scrolling q-list

--- app/views/queues.blade.php
+++ app/views/queues.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.main')
 
 @section('content')
+	<script>
+		$(function() {
+			var height = $(window).height() * 0.75;
+			$('#q-list').css('max-height', height);
+		});
+	</script>
 
 	<h1>Select a Queue</h1>
-	<div class="q-list">
+	<div id="q-list">
 		@for($i=0; $i<$q_count; $i++)
 			@include('blocks/q-listing')
 		@endfor
